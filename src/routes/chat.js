ecee1490f80c5771802d9b54ee9a32f5fc9329a9
Rudra Sett <rudro.sett@gmail.com>
2024-08-30 15:15:17 -0400

    updated to use new retrieval system

--- src/routes/chat.js
+++ src/routes/chat.js
@@ -1,82 +1,39 @@
 
 module.exports = async (req, res) => {
-    
 
     const projectId = 'affable-seat-433116-u6';
-    const location = 'global';              // Options: 'global', 'us', 'eu'
-    const collectionId = 'default_collection';     // Options: 'default_collection'
-    const dataStoreId = 'ride-data_1724179572038_gcs_store'       // Create in Cloud Console
-    const servingConfigId = 'default_serving_config';      // Options: 'default_config'
-    
+
     const userPrompt = req.body.message;
     const chatHistory = req.body.history;
 
-    const { SearchServiceClient } = require('@google-cloud/discoveryengine');
     const { VertexAI } = require('@google-cloud/vertexai');
+    const axios = require("axios");    
 
-    // For more information, refer to:
-    // https://cloud.google.com/generative-ai-app-builder/docs/locations#specify_a_multi-region_for_your_data_store
-    const apiEndpoint =
-        location === 'global'
-            ? 'discoveryengine.googleapis.com'
-            : `${location}-discoveryengine.googleapis.com`;
-
-    // Instantiates a client
-    const client = new SearchServiceClient({ apiEndpoint: apiEndpoint });
-
-    async function search() {
-        // The full resource name of the search engine serving configuration.        
-        // You must create a search engine in the Cloud Console first.
-        const name = client.projectLocationCollectionDataStoreServingConfigPath(
-            projectId,
-            location,
-            collectionId,
-            dataStoreId,
-            servingConfigId
-        );
-
-        const request = {
-            pageSize: 5,
-            query: userPrompt,
-            servingConfig: name,
-            queryExpansionSpec: { condition: "AUTO" },
-            spellCorrectionSpec: { mode: "AUTO" },
-            contentSearchSpec: {
-                extractiveContentSpec: {
-                    maxExtractiveSegmentCount: 1
+    async function search() {        
+        // custom RAG
+        const response = await axios.post(
+            `https://gcp-chatbot-vectordb-g5vgkh3bjq-uc.a.run.app/retrieve`,{
+                'query': userPrompt,                    
+            },
+            {
+                headers: {                    
+                    'Content-Type': 'application/json'
+                },
+                body: {
+                    'query': userPrompt,                    
+                },
+                params: {
+                    'query': userPrompt,
                 }
-            }
-        };
+            });
 
-        const IResponseParams = {
-            ISearchResult: 0,
-            ISearchRequest: 1,
-            ISearchResponse: 2,
-        };
-
-        // console.log(request);
-
-        // Perform search request
-        const response = await client.search(request, {
-            // Warning: Should always disable autoPaginate to avoid iterate through all pages.
-            //
-            // By default NodeJS SDK returns an iterable where you can iterate through all
-            // search results instead of only the limited number of results requested on
-            // pageSize, by sending multiple sequential search requests page-by-page while
-            // iterating, until it exhausts all the search results. This will be unexpected and
-            // may cause high Search API usage and long wait time, especially when the matched
-            // document numbers are huge.
-            autoPaginate: false,
-        });
-
-        // console.log(response);
-        const results = response[IResponseParams.ISearchResult];
+        console.log(response);        
+        const results = response.data.snippets;
 
         let fullContent = "";
-        for (const result of results) {
-            // console.log(result.document.derivedStructData.fields)
-            try {
-                fullContent = fullContent.concat(result.document.derivedStructData.fields.extractive_segments.listValue.values[0].structValue.fields.content.stringValue)
+        for (const result of results) {            
+            try {                
+                fullContent = fullContent.concat(result);
             } catch (e) {
                 fullContent = fullContent.concat("No data found!");
                 console.error(e);
@@ -88,23 +45,28 @@ module.exports = async (req, res) => {
 
     const context = await search();
     let systemPrompt = `You are an AI chatbot for the RIDE, an MBTA paratransit service. You will help customer service representatives respond to user complaints and queries.
-    Answer questions based on your knowledge and nothing more. If you are unable to decisively answer a question, direct them to customer service. Do not make up information outside of your given information.
+    Answer questions based on your knowledge and nothing more. If you are unable to decisively answer a question, provide whatever information you have, and then direct them to customer service.
+    Do not make up information outside of your given information.
     Customer service is needed if it is something you cannot answer. Requests for fare history require customer service, as do service complaints like a rude driver or late pickup.
     Highly-specific situations will also require customer service to step in. Remember that RIDE Flex and RIDE are not the same service. 
+
     Phone numbers:
     TRAC (handles scheduling/booking, trip changes/cancellations, anything time-sensitive): 844-427-7433 (voice/relay) 857-206-6569 (TTY)
     Mobility Center (handles eligibility questions, renewals, and changes to mobility status): 617-337-2727 (voice/relay)
     MBTA Customer support (handles all other queries): 617-222-3200 (voice/relay)
-    ${context}`;  
+
+    Please use this information to respond as thoroughly and completely as you can (long answers are good, use as much information as you have), and format your answers when possible.
+    Remember, verbosity is good. This is information you have available to use:
+    Knowledge: ${context}`;
 
     const vertexAI = new VertexAI({ project: projectId, location: 'us-central1' });
 
-    const textModel =  'gemini-1.5-pro';
+    const textModel = 'gemini-1.5-flash';
 
     // Instantiate Gemini models
     const generativeModel = vertexAI.getGenerativeModel({
-        model: textModel,        
-        generationConfig: { maxOutputTokens: 2048 },
+        model: textModel,
+        generationConfig: { maxOutputTokens: 2048, temperature: 0.01 },
         systemInstruction: {
             role: 'system',
             parts: [{ text: systemPrompt }]
@@ -126,13 +88,13 @@ module.exports = async (req, res) => {
     }
 
     async function streamGenerateContent(prompt) {
-        
+
         let parsedHistory = [];
 
         try {
             // parsedHistory = JSON.parse(chatHistory);
             parsedHistory = assembleHistory(chatHistory);
-        } catch (e){
+        } catch (e) {
             console.error("Could not parse history!")
             console.error(e);
         }
@@ -141,7 +103,7 @@ module.exports = async (req, res) => {
             contents: [...parsedHistory, { role: 'user', parts: [{ text: prompt }] }],
         };
         const streamingResult = await generativeModel.generateContentStream(request);
-        for await (const item of streamingResult.stream) {            
+        for await (const item of streamingResult.stream) {
             res.write(item.candidates[0].content.parts[0].text
             )
         }
